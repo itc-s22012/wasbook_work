2b01c3ded89fb58e3522f3c04fedf7e5760b95de
itc-s22012 <s22012@std.it-college.ac.jp>
2024-07-25 17:02:40 +0900

    4.7

---
+++ 4.7/47-002.php
@@ -0,0 +1,11 @@
+<?php
+ $id = filter_input(INPUT_POST, 'id');
+ $pwd = filter_input(INPUT_POST, 'pwd');
+ $url = filter_input(INPUT_POST, 'url');
+if (! empty($id) && ! empty($pwd)) {
+header('Location: ' . $url);
+exit();
+}
+?><body>
+<a href="47-001.php">再ログイン</a>
+</body>
