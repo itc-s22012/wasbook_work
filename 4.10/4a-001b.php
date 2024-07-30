627c293d78ca661d257556ccc8edee1311999f7a
itc-s22012 <s22012@std.it-college.ac.jp>
2024-07-30 16:09:18 +0900

    4.10終わり

---
+++ 4.10/4a-001b.php
@@ -0,0 +1,8 @@
+<?php
+ define('TMPLDIR', '/var/www/html/work/4.10/tmpl/');
+ $tmpl = basename(filter_input(INPUT_GET, 'template'));
+?>
+<body>
+<?php readfile(TMPLDIR . $tmpl . '.html'); ?>
+メニュー(以下略)
+</body>
